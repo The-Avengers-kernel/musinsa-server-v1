3a87c8af6664d53e93c03baca0733aed62b699ab
mmingoo <aldrn0323@naver.com>
2025-09-13 21:31:07 +0900

    Initial commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+-- 초기 테스트 데이터 삽입
+INSERT INTO users (name, email) VALUES ('홍길동', 'hong@musinsa.com');
+INSERT INTO users (name, email) VALUES ('김무신', 'kim@musinsa.com');
+INSERT INTO users (name, email) VALUES ('이패션', 'lee@musinsa.com');
