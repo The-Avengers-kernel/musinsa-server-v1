79bf094fa73fb27cdf03c142a3a2b03f7a5f8397
kingjinyong <wlsdydwhswkf@gmail.com>
2025-09-16 12:00:19 +0900

    feat: ReviewImages 테이블 엔티티 작성

---
+++ src/main/java/com/avengers/musinsa/review/ReviewImages.java
@@ -0,0 +1,22 @@
+package com.avengers.musinsa.review;
+
+public class ReviewImages {
+
+    private Long reviewImageId;
+
+    private Reviews reviews;
+
+    private String imagesUrl;
+
+    public String getImagesUrl() {
+        return imagesUrl;
+    }
+
+    public Long getReviewImageId() {
+        return reviewImageId;
+    }
+
+    public Reviews getReviews() {
+        return reviews;
+    }
+}
--- src/main/java/com/avengers/musinsa/review/Reviews.java
+++ src/main/java/com/avengers/musinsa/review/Reviews.java
@@ -15,4 +15,32 @@ public class Reviews {
     private String purchaseOptions;
 
     private Integer helpCount;
+
+    public String getContent() {
+        return content;
+    }
+
+    public Integer getHelpCount() {
+        return helpCount;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public Products getProduct() {
+        return product;
+    }
+
+    public String getPurchaseOptions() {
+        return purchaseOptions;
+    }
+
+    public Long getReviewId() {
+        return reviewId;
+    }
+
+    public Users getUser() {
+        return user;
+    }
 }
