e8c8cd928950f7f9c9ec0e9464f1cc63241c4868
kingjinyong <wlsdydwhswkf@gmail.com>
2025-09-16 11:09:18 +0900

    feat: paymentMethods 테이블 엔티티 작성

--- src/main/java/com/avengers/musinsa/order/PaymentMethods.java
+++ src/main/java/com/avengers/musinsa/order/PaymentMethods.java
@@ -1,4 +1,16 @@
 package com.avengers.musinsa.order;
 
 public class PaymentMethods {
+
+    private Long paymentMethodId;
+
+    private String paymentName;
+
+    public Long getPaymentMethodId() {
+        return paymentMethodId;
+    }
+
+    public String getPaymentName() {
+        return paymentName;
+    }
 }
