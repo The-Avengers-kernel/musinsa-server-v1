a87b3c6b4bc3ce8f11183d2ef42a4486449446ba
kingjinyong <wlsdydwhswkf@gmail.com>
2025-09-16 12:10:54 +0900

    feat: UserBrandLikes 테이블 엔티티 작성

---
+++ src/main/java/com/avengers/musinsa/review/UserBrandLikes.java
@@ -0,0 +1,22 @@
+package com.avengers.musinsa.review;
+
+public class UserBrandLikes {
+
+    private Long userBrandLikeId;
+
+    private Brands brand;
+
+    private Users user;
+
+    public Brands getBrand() {
+        return brand;
+    }
+
+    public Users getUser() {
+        return user;
+    }
+
+    public Long getUserBrandLikeId() {
+        return userBrandLikeId;
+    }
+}
