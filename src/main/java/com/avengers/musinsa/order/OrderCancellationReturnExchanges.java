fd501515b2ca5b66694b7df98fc0ee6e5d9ad22f
kingjinyong <wlsdydwhswkf@gmail.com>
2025-09-16 11:00:54 +0900

    feat: OrderCancellationReturnExchanges 테이블 엔티티 getter 작성

--- src/main/java/com/avengers/musinsa/order/OrderCancellationReturnExchanges.java
+++ src/main/java/com/avengers/musinsa/order/OrderCancellationReturnExchanges.java
@@ -45,4 +45,88 @@ public class OrderCancellationReturnExchanges {
     private String noticesId;
 
     private Timestamp completionDate;
+
+    public String getCancellationReturnExchangeReason() {
+        return cancellationReturnExchangeReason;
+    }
+
+    public String getCancellationReturnExchangeStatus() {
+        return cancellationReturnExchangeStatus;
+    }
+
+    public Timestamp getCompletionDate() {
+        return completionDate;
+    }
+
+    public String getExchangeDestination() {
+        return exchangeDestination;
+    }
+
+    public String getExchangeMethod() {
+        return exchangeMethod;
+    }
+
+    public String getExchangePickupLocation() {
+        return exchangePickupLocation;
+    }
+
+    public Integer getExchangeShippingFee() {
+        return exchangeShippingFee;
+    }
+
+    public String getNoticesId() {
+        return noticesId;
+    }
+
+    public Long getOrderCancellationReturnExchangesId() {
+        return orderCancellationReturnExchangesId;
+    }
+
+    public OrderItems getOrderItem() {
+        return orderItem;
+    }
+
+    public String getRefundMethod() {
+        return refundMethod;
+    }
+
+    public String getRefundStatus() {
+        return refundStatus;
+    }
+
+    public String getReshipmentCarrier() {
+        return reshipmentCarrier;
+    }
+
+    public String getReshipTrackingNumber() {
+        return reshipTrackingNumber;
+    }
+
+    public String getReturnCarrier() {
+        return returnCarrier;
+    }
+
+    public Timestamp getReturnDate() {
+        return returnDate;
+    }
+
+    public String getReturnDestination() {
+        return returnDestination;
+    }
+
+    public String getReturnMethod() {
+        return returnMethod;
+    }
+
+    public String getReturnPickupLocation() {
+        return returnPickupLocation;
+    }
+
+    public Integer getReturnShippingFee() {
+        return returnShippingFee;
+    }
+
+    public String getReturnTrackingNumber() {
+        return returnTrackingNumber;
+    }
 }
