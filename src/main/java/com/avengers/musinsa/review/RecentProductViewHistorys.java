4b80649de178735d3f168e8ddabba927ce20091a
kingjinyong <wlsdydwhswkf@gmail.com>
2025-09-16 12:17:37 +0900

    feat: RecentProductViewHistorys 테이블 엔티티 작성

---
+++ src/main/java/com/avengers/musinsa/review/RecentProductViewHistorys.java
@@ -0,0 +1,30 @@
+package com.avengers.musinsa.review;
+
+import java.sql.Timestamp;
+
+public class RecentProductViewHistory {
+
+    private Long RecentProductViewHistoryId;
+
+    private Users user;
+
+    private Products product;
+
+    private Timestamp viewTime;
+
+    public Products getProduct() {
+        return product;
+    }
+
+    public Long getRecentProductViewHistoryId() {
+        return RecentProductViewHistoryId;
+    }
+
+    public Users getUser() {
+        return user;
+    }
+
+    public Timestamp getViewTime() {
+        return viewTime;
+    }
+}
