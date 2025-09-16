b45eb09cc3b4e7377c5c4d757f12cb382509c830
kingjinyong <wlsdydwhswkf@gmail.com>
2025-09-16 11:59:07 +0900

    feat: Reviews 테이블 엔티티 작성

---
+++ src/main/java/com/avengers/musinsa/review/Reviews.java
@@ -0,0 +1,18 @@
+package com.avengers.musinsa.review;
+
+public class Reviews {
+
+    private Long reviewId;
+
+    private Products product;
+
+    private Users user;
+
+    private String nickname;
+
+    private String content;
+
+    private String purchaseOptions;
+
+    private Integer helpCount;
+}
