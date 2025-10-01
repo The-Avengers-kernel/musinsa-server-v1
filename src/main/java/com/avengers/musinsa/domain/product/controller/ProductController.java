0d5783d82e727ca4508d4057856d294ca2f20292
kingjinyong <wlsdydwhswkf@gmail.com>
2025-10-01 11:28:15 +0900

    feat:리뷰 작성 기능 구현

--- src/main/java/com/avengers/musinsa/domain/product/controller/ProductController.java
+++ src/main/java/com/avengers/musinsa/domain/product/controller/ProductController.java
@@ -3,7 +3,9 @@ package com.avengers.musinsa.domain.product.controller;
 import com.avengers.musinsa.domain.product.dto.response.*;
 import com.avengers.musinsa.domain.product.dto.search.SearchResponse;
 import com.avengers.musinsa.domain.product.entity.Gender;
+import com.avengers.musinsa.domain.product.service.ProductService;
 import com.avengers.musinsa.domain.product.service.ProductServiceImpl;
+import com.avengers.musinsa.domain.review.dto.Request.RequestCreateReview;
 import java.util.List;
 
 import com.avengers.musinsa.domain.user.auth.jwt.TokenProviderService;
@@ -18,19 +20,20 @@ import org.springframework.web.server.ResponseStatusException;
 @RequiredArgsConstructor
 public class ProductController {
 
-    private final ProductServiceImpl productService;
+    private final ProductService productService;
     private final TokenProviderService tokenProviderService;
 
     // "해당 productId 상품 정보를 반환해준다.
     @GetMapping("{productId}")
-    public ProductDetailResponse getDetailProduct(@PathVariable Long productId, @CookieValue(value = "Authorization", required = false) String authorizationHeader) {
+    public ProductDetailResponse getDetailProduct(@PathVariable Long productId,
+                                                  @CookieValue(value = "Authorization", required = false) String authorizationHeader) {
 
         if (authorizationHeader == null || authorizationHeader.isEmpty()) {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Authorization token is missing");
         } else {
             Long userId = tokenProviderService.getUserIdFromToken(authorizationHeader);
 
-            return productService.getProductById(productId,userId);
+            return productService.getProductById(productId, userId);
         }
     }
 
@@ -54,8 +57,8 @@ public class ProductController {
     }
 
     //카테고리 선택 시 상품 목록 조회되는 화면
-      @GetMapping("/category/{categoryId}")
-      public ResponseEntity<List<ProductByCategoryResponse>> getProductsByCategory(@PathVariable Long categoryId) {
+    @GetMapping("/category/{categoryId}")
+    public ResponseEntity<List<ProductByCategoryResponse>> getProductsByCategory(@PathVariable Long categoryId) {
         System.out.println("category_id = " + categoryId);
         List<ProductByCategoryResponse> products = productService.getProductsByCategory(categoryId);
         return ResponseEntity.ok(products);
@@ -67,9 +70,20 @@ public class ProductController {
         return productService.getProductReviews(productId);
     }
 
+    // 상품 리뷰 작성
+    @PostMapping("{productId}/reviews/write")
+    public ResponseEntity<?> createProductReview(@PathVariable Long productId,
+                                                 @CookieValue(value = "Authorization", required = false) String authorizationHeader,
+                                                 @RequestBody RequestCreateReview requestCreateReview) {
+        Long userId = tokenProviderService.getUserIdFromToken(authorizationHeader);
+        productService.createProductReview(productId, userId, requestCreateReview);
+        return ResponseEntity.ok().build();
+    }
+
     // 상품상세 사이즈 리스트 조회
     @GetMapping("{productId}/detail-size-list")
-    public Object getProductDetailSizeList(@PathVariable Long productId, @CookieValue(value = "Authorization", required = false) String authorizationHeader) {
+    public Object getProductDetailSizeList(@PathVariable Long productId,
+                                           @CookieValue(value = "Authorization", required = false) String authorizationHeader) {
         if (authorizationHeader == null || authorizationHeader.isEmpty()) {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Authorization token is missing");
         } else {
