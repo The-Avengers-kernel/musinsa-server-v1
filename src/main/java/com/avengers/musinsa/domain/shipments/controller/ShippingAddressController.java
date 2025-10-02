9aede47453f7ccd81db7b68e4f523c3367969afe
pikapika <aoxbtksgo13@naver.com>
2025-10-02 20:04:25 +0900

    배송지 추가 수정 삭제 - 배송지 추가 (완료) - 수정은 화면만 띄움 (구현중) - 삭제 구현중 - 주소검색 연동안함

--- src/main/java/com/avengers/musinsa/domain/shipments/controller/ShippingAddressController.java
+++ src/main/java/com/avengers/musinsa/domain/shipments/controller/ShippingAddressController.java
@@ -1,20 +1,22 @@
-//package com.avengers.musinsa.domain.shipments.controller;
-//
-//import com.avengers.musinsa.domain.shipments.dto.ShippingAddressOrderDTO;
-//import com.avengers.musinsa.domain.shipments.service.ShippingAddressService;
-//import com.avengers.musinsa.domain.user.auth.jwt.JWTUtil;
-//import com.avengers.musinsa.domain.user.auth.jwt.TokenProviderService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//
-//@RestController
-//@RequiredArgsConstructor
-//@RequestMapping("/api/v1")
-//public class ShippingAddressController {
-//    private final ShippingAddressService shippingAddressService;
+package com.avengers.musinsa.domain.shipments.controller;
+
+import com.avengers.musinsa.domain.shipments.dto.ShippingAddressCreateDTO;
+import com.avengers.musinsa.domain.shipments.dto.ShippingAddressOrderDTO;
+import com.avengers.musinsa.domain.shipments.service.ShippingAddressService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
+public class ShippingAddressController {
+    private final ShippingAddressService shippingAddressService;
+
+    //제외
 //    private final TokenProviderService tokenProviderService;
 //    private final JWTUtil jWTUtil;
 //
@@ -40,4 +42,49 @@
 //            return ResponseEntity.status(500).build();
 //        }
 //    }
-//}
+
+    //배솧지 추가\
+
+    @PostMapping("/add")
+    public ResponseEntity<String> addShippingAddress(@RequestBody ShippingAddressCreateDTO shippingAddressCreate) {
+        try {
+            shippingAddressService.insertShippingAddress(shippingAddressCreate);
+            return ResponseEntity.ok("배송지가 저장되었습니다./");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("저장 실패: " + e.getMessage());
+        }
+    }
+
+
+    //배송지 수정
+    @PutMapping("/update/{userId}/{shippingAddressId}")
+    public String updateShippingAddress(@RequestBody ShippingAddressCreateDTO shippingAddressCreate,
+                                        @PathVariable("userId") Long userId,
+                                        @PathVariable("shippingAddressId") Long shippingAddressId) {
+        shippingAddressService.updateShippingAddress(shippingAddressCreate, userId, shippingAddressId);
+        return "ok";
+    }
+
+
+    //배송지 삭제
+    @DeleteMapping("/delete/{shippingAddressId}/{userId}")
+    public String deleteShippingAddress(@PathVariable("shippingAddressId") Long shippingAddressId,
+                                        @PathVariable("userId") Long userId) {
+        shippingAddressService.deleteShippingAddress(shippingAddressId, userId);
+        return "ok";
+    }
+
+    //기본 배송지 변경
+    @PutMapping("/change-default/{userId}/{shippingAddressId}")
+    public String changeDefaultShippingAddress(@PathVariable("userId") Long userId,
+                                                @PathVariable("shippingAddressId") Long shippingAddressId){
+        shippingAddressService.changeDefaultShippingAddress(userId, shippingAddressId);
+
+        return "ok";
+    }
+
+
+
+
+}
