c62529b7ddcfbe9755283b3e6113f27a76606fe1
kingjinyong <wlsdydwhswkf@gmail.com>
2025-10-03 16:59:51 +0900

    structure: 파일 구조 변경 및 병합 - search, order 부분 수정

--- src/main/java/com/avengers/musinsa/domain/search/repository/SearchLogRepositoryImpl.java
+++ src/main/java/com/avengers/musinsa/domain/search/repository/SearchLogRepositoryImpl.java
@@ -2,7 +2,7 @@ package com.avengers.musinsa.domain.search.repository;
 
 import com.avengers.musinsa.domain.search.dto.SearchSaveDto;
 import com.avengers.musinsa.domain.search.entity.SearchLog;
-import com.avengers.musinsa.mapper.SearchLogMapper;
+import com.avengers.musinsa.mapper.SearchMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -12,24 +12,24 @@ import org.springframework.stereotype.Repository;
 public class SearchLogRepositoryImpl implements SearchLogRepository {
 
     //SearchLogMapper 주입
-    private final SearchLogMapper searchLogMapper;
+    private final SearchMapper searchMapper;
 
     // 검색 로그 저장 후 생성된 logId 반환
     @Override
     public Long save(SearchLog searchLog) {
-        searchLogMapper.insertSearchLog(searchLog); // 검색 로그 저장
+        searchMapper.insertSearchLog(searchLog); // 검색 로그 저장
         return searchLog.getSearchLogId(); // 엔티티의 logId 반환
     }
 
     @Override
     public Long saveSearchBrandLog(SearchSaveDto.searchBrandLogSaveDto brandLogSaveDto) {
-        searchLogMapper.insertSearchBrandLog(brandLogSaveDto); // 검색 로그 저장
+        searchMapper.insertSearchBrandLog(brandLogSaveDto); // 검색 로그 저장
         return brandLogSaveDto.getSearchBrandLogSaveId(); // 엔티티의 logId 반환
     }
 
     @Override
     public Long saveSearchKeywordLog(SearchSaveDto.searchKeywordLogSaveDto keywordLogSaveDto) {
-        searchLogMapper.insertSearchKeywordLog(keywordLogSaveDto); // 검색 로그 저장
+        searchMapper.insertSearchKeywordLog(keywordLogSaveDto); // 검색 로그 저장
         return keywordLogSaveDto.getSearchKeywordLogSaveId(); // 엔티티의 logId 반환
     }
 
--- src/main/java/com/avengers/musinsa/mapper/SearchMapper.java
+++ src/main/java/com/avengers/musinsa/mapper/SearchMapper.java
@@ -2,10 +2,15 @@ package com.avengers.musinsa.mapper;
 
 import com.avengers.musinsa.domain.search.dto.SearchSaveDto;
 import com.avengers.musinsa.domain.search.entity.SearchLog;
+import com.avengers.musinsa.domain.search.response.SearchKeywordResponseDTO;
+import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
-public interface SearchLogMapper {
+public interface SearchMapper {
+
+    List<SearchKeywordResponseDTO> getRecentSearches(@Param("user_id") Long userId);
 
     //검색 로그 저장 - 자동생성된 ID를 엔티티에 설정
     void insertSearchLog(SearchLog searchLog);
